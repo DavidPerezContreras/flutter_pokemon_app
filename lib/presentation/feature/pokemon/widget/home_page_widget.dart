de451a660f831f8b2d1ac98f7723416480fcb745
DavidPerezContreras <david.perez.dam2@gmail.com>
2023-11-08 20:20:05 +0100

    appbar

--- lib/presentation/feature/pokemon/widget/home_page_widget.dart
+++ lib/presentation/feature/pokemon/widget/home_page_widget.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_app/model/pokemon.dart';
 import 'package:flutter_app/presentation/feature/pokemon/widget/page/about_me_page.dart';
 import 'package:flutter_app/presentation/feature/pokemon/widget/page/all_page.dart';
 import 'package:flutter_app/presentation/feature/pokemon/widget/page/favorites_page.dart';
 import 'package:flutter_app/presentation/feature/pokemon/widget/pokemon_list_widget.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+
 class MyNavigationPage extends StatefulWidget {
   MyNavigationPage({Key? key}) : super(key: key);
 
@@ -17,75 +19,66 @@ class _MyNavigationPageState extends State<MyNavigationPage> {
   int _navigationIndex = 0;
   late List<Page<dynamic>> pages;
 
-  MaterialPageRoute materialAllPageRoute= MaterialPageRoute(builder: (BuildContext context)=> const AllPage());
+  MaterialPageRoute materialAllPageRoute =
+      MaterialPageRoute(builder: (BuildContext context) => const AllPage());
 
   @override
   void initState() {
     super.initState();
-
   }
 
   @override
   Widget build(BuildContext context) {
-
-    switch(_navigationIndex){
-      case 0:
-    pages = [
-      MaterialPage(child: AllPage()),
-      MaterialPage(child: FavoritesPage()),
-      MaterialPage(child: AboutMePage()),
-    ];
-      break;
-      case 1:
-    pages = [
-      MaterialPage(child: AllPage()),
-      MaterialPage(child: FavoritesPage()),
-      MaterialPage(child: AboutMePage()),
-    ];
-      break;
-      case 2:
-    pages = [
-      MaterialPage(child: AllPage()),
-      MaterialPage(child: FavoritesPage()),
-      MaterialPage(child: AboutMePage()),
-    ];
-      break;
-    }
-    pages = [
-      MaterialPage(child: AllPage()),
-      MaterialPage(child: FavoritesPage()),
-      MaterialPage(child: AboutMePage()),
-    ];
-
-
     return Scaffold(
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: _navigationIndex,
-        onTap: (int index) {
-          
-          setState(() {
-            _navigationIndex = index;
-          });
-        },
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Home',
+        appBar: AppBar(
+          title: const Text("flutter_pokemon_app"),
+          actions: <Widget>[
+            IconButton(
+              icon: const Icon(Icons.comment),
+              tooltip: 'Comment Icon',
+              onPressed: () {},
+            ), //IconButton
+            IconButton(
+              icon: const Icon(Icons.settings),
+              tooltip: 'Setting Icon',
+              onPressed: () {},
+            ), //IconButton
+          ], //<Widget>[]
+          backgroundColor: Colors.greenAccent[400],
+          elevation: 50.0,
+          leading: IconButton(
+            icon: const Icon(Icons.menu),
+            tooltip: 'Menu Icon',
+            onPressed: () {},
           ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.favorite),
-            label: 'Favorites',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.person),
-            label: 'About me',
-          ),
-        ],
-        selectedItemColor: Colors.blue,
-      ),
-      body: IndexedStack(
-        index: _navigationIndex,
-        children: [AllPage(),FavoritesPage(),AboutMePage()],)
-    );
+          systemOverlayStyle: SystemUiOverlayStyle.light,
+        ), //AppBar,
+        bottomNavigationBar: BottomNavigationBar(
+          currentIndex: _navigationIndex,
+          onTap: (int index) {
+            setState(() {
+              _navigationIndex = index;
+            });
+          },
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.favorite),
+              label: 'Favorites',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.person),
+              label: 'About me',
+            ),
+          ],
+          selectedItemColor: Colors.blue,
+        ),
+        body: IndexedStack(
+          index: _navigationIndex,
+          children: [AllPage(), FavoritesPage(), AboutMePage()],
+        ));
   }
 }
